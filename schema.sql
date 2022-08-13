d03589e090cac0e6e4d851cb3e6a47a9f6c7924b
cristinagzz <cristy_gzzz@gotmail.com>
2022-08-13 11:21:03 -0500

    Update in Classwork

--- schema.sql
+++ schema.sql
@@ -29,23 +29,28 @@ CREATE TABLE salaries (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no,from_date)
 );
 CREATE TABLE Dept_Emp (
-  dept_no VARCHAR NOT NULL,
   emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no,dept_no)
 );
 CREATE TABLE Titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
 );
 SELECT * FROM departments
+SELECT * FROM employees
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM salaries
+SELECT * FROM titles
